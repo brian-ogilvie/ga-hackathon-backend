f37c71401c2b3290f442fbf1495fafed0e29d376
AlexGaiser <alexcgaiser@gmail.com>
2019-03-30 15:33:16 -0400

    added model_maker.py

---
+++ maritime_backend/model_maker.py
@@ -0,0 +1,13 @@
+import csv
+import pandas as pd 
+
+df = pd.read_csv("updated_data.csv")
+
+print(df.head(0))
+
+for i in df.head(0):
+    print(
+        i + ''' = IntegerField()'''
+    
+    
+    )
